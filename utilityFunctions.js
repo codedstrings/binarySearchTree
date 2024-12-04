4945d9a2b9133338fbd0f6315f31103e93523d73
codedstrings <mridhulml@gmail.com>
2024-12-04 19:44:09 +0530

    feat: add prettyPrint() to print bst
    - add prettyPrint in new file utitlityFunctions.js

---
+++ utilityFunctions.js
@@ -0,0 +1,15 @@
+//function that shows the binary tree in tree like structure in console
+function prettyPrint(node, prefix = "", isLeft = true) {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+}
+
+export { prettyPrint };
